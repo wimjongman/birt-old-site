01cffcb6cbf74ef00d68be3014a8c505a43dfbd5
droy <droy>
2004-12-01 19:19:15 +0000

    *** empty log message ***

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Created on Dec 1, 2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
-?>
-Hello, world!!
