7332ed11b6c402abf1ef5de09857bfb9218611e0
progers <progers>
2005-02-26 02:07:21 +0000

    Download portion of new website structure

---
+++ view.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Created on Feb 25, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+require "include/viewer.inc";
+viewer( );
+?>
