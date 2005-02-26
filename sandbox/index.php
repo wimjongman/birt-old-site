05aaa646bb6a1a2a29b0ac7ac0f49b693b28fb60
progers <progers>
2005-02-26 01:00:05 +0000

    Test prior to new BIRT website structure.

---
+++ sandbox/index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Created on Feb 25, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
+<html>
+<body>
+It works!
+Test: <?php echo "OK"; ?>
+</body>
+</html>
