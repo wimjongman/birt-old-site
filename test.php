d800f079782152e91dc2378dbefe424e02311f02
progers <progers>
2005-02-28 04:28:46 +0000

    Roll out of 1.0M1 web site

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Created on Feb 27, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 transitional//EN">
+<html>
+
+<head>
+<title>Test</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<?php phpinfo(); ?> 
+
+</body>
+</html>
