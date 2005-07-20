0abdbc4b6a6ccc9a06ab32363cb411e7223a6c5d
mcoggins <mcoggins>
2005-07-20 22:20:11 +0000

    More to get uploads to work.

--- wiki/local/Release20Specs.php
+++ wiki/local/Release20Specs.php
@@ -5,4 +5,5 @@ PatchCommentBrowseFmt();
 $EnableUpload = 1;
 $UploadMaxSize = 500000;
 $UploadDir = "/home/data/httpd/writable/birt/wiki.d/uploads";
+$UploadUrlFmt = "http://www.eclipse.org/birt/wiki/wiki.d/uploads";
 
