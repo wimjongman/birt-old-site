03979fbe281054bc3c89ba10eabf62e2624513c5
mcoggins <mcoggins>
2005-08-24 15:45:23 +0000

    engine changes

--- deploy/RunReport.java
+++ deploy/RunReport.java
@@ -24,9 +24,9 @@ import org.eclipse.birt.report.engine.api.ReportEngine;
 
 /**
  * Runs a BIRT report and writes the output to a directory.
- * 
+ *
  * RunReport report-name -[h|p] [-i] param=value
- * 
+ *
  * -h HTML (default)
  * -p PDF
  * -i Display report info.
@@ -37,7 +37,7 @@ public class RunReport
 {
 
 	// Command-line main to parse arguments described above.
-	
+
 	public static void main(String[] args)
 	{
 		int i = 0;
@@ -49,10 +49,10 @@ public class RunReport
 		String reportName = args[i++];
 		String format = null;
 		boolean showInfo = false;
-		
+
 		// Stoare parameter values into a hash map to later pass to the
 		// BIRT report engine.
-		
+
 		HashMap params = new HashMap( );
 		for ( ;  i < args.length;  i++ )
 		{
@@ -83,7 +83,7 @@ public class RunReport
 				// for string parameters. If you have date or number parameters,
 				// your application should parse them, then put the corresponding
 				// objects into the hash map.
-				
+
 				String arg = args[i];
 				int posn = arg.indexOf( '=' );
 				if ( posn == -1 )
@@ -98,32 +98,32 @@ public class RunReport
 		}
 		run( reportName, params, format, showInfo );
 	}
-	
+
 	public static void usage( )
 	{
 		System.out.println( "java RunReport rptName [-h|p]\n" );
 		System.out.println( "               param=value ..." );
 	}
-	
+
 	// Example function to run a report given the report name, parameters and
 	// output type. The output is put into a file in the same directory as the
 	// report itself.
-	
+
 	public static void run( String name, HashMap params,
 							String format, boolean showInfo )
 	{
 		// The EngineConfig is used to set up engine-wide configuration
 		// information.
-		
+
 		EngineConfig config = new EngineConfig( );
-		
+
 		// The Engine home is the location of the BIRT engine runtime.
 		// M3 Note: Use the report engine installed as part of the BIRT web app
 		// used to preview reports. Alter this path to the location of your
 		// Eclipse installation.
-		
-		config.setEngineHome( "/Program Files/eclipse/plugins/org.eclipse.birt.report.viewer_0.0.1/birt/WEB-INF" );
-		
+
+		config.setEngineHome( "c:\\eclipse3.1\\eclipse\\plugins\\org.eclipse.birt.report.viewer_1.0.1\\birt" );
+
 		// HTML reports can have embedded images or charts. Set up the configuration
 		// required to write these into a directory in the output directory.
 		// The HTMLCompleteImageHandler is one provided by BIRT for this purpose.
@@ -137,11 +137,11 @@ public class RunReport
 
 		// Create the report engine itself. This engine can be used to run
 		// multiple reports.
-		
+
 		ReportEngine engine = new ReportEngine( config );
-		
+
 		// Open the report design.
-		
+
 		IReportRunnable report = null;
 		try
 		{
@@ -153,20 +153,20 @@ public class RunReport
 			engine.destroy( );
 			return;
 		}
-		
+
 		if ( showInfo )
 		{
 			showReportInfo( engine, report );
 		}
-		
+
 		// Create a task to run the report and convert the output to HTML
 		// or PDF.
-		
+
 		if ( format != null )
 		{
-		
+
 			parseParams( engine, report, params );
-			
+
 			IRunAndRenderTask task = engine.createRunAndRenderTask( report );
 			HTMLRenderOption options = new HTMLRenderOption( );
 			options.setOutputFormat( format );
@@ -174,9 +174,9 @@ public class RunReport
 			options.setOutputFileName( output );
 			task.setRenderOption( options );
 			task.setParameterValues( params );
-			
+
 			// Run the report.
-			
+
 			try
 			{
 				task.run( );
@@ -187,17 +187,17 @@ public class RunReport
 				System.err.println( e1.toString( ) );
 			}
 		}
-			
+
 		engine.destroy( );
 	}
-	
+
 	// Display properties of the report and the list of report parameters.
-	
+
 	static void showReportInfo( ReportEngine engine, IReportRunnable report )
 	{
 		// Display some of the general report properties.
 		// See the ROM Design specification for a list of properties.
-		
+
 		System.out.print( "Report Name: " + report.getReportName() +
 						  "\nTitle: " + report.getProperty( "title" ) +
 						  "\nComments: " + report.getProperty( "comments" ) +
@@ -205,11 +205,11 @@ public class RunReport
 						  "\nAuthor: " + report.getProperty( "author" ) +
 						  "\nCreated By: " + report.getProperty( "createdBy" ) +
 						  "\n" );
-		
+
 		// Display the list of report parameters with their groups.
 		// Parameters defined outside a group appear inside a default, unnamed
 		// group.
-				
+
 		IGetParameterDefinitionTask task = engine.createGetParameterDefinitionTask( report );
 		Collection params = task.getParameterDefns( true );
 		if ( params.isEmpty() )
@@ -217,7 +217,7 @@ public class RunReport
 			System.out.print( "This report has no parameters.\n" );
 			return;
 		}
-		
+
 		Iterator iter = params.iterator( );
 		while ( iter.hasNext( ) )
 		{
@@ -235,12 +235,12 @@ public class RunReport
 			}
 			else
 				showParam( task, param, "" );
-		}	
+		}
 	}
 
 	// Show information for one parameter.
 	// Release 1 supports only scalar (single-value) parameters.
-	
+
 	static void showParam( IGetParameterDefinitionTask task,
 						   IParameterDefnBase defn,
 						   String prefix )
@@ -252,23 +252,23 @@ public class RunReport
 		System.out.print( prefix + "  Default Value: " );
 
 		// The default value is an expression. Evaluate it.
-		
+
 		Object value = task.getDefaultValue( param );
 		System.out.println( value == null ? "none" : value.toString( ) );
 	}
-	
+
 	static void parseParams( ReportEngine engine, IReportRunnable report, HashMap values )
 	{
 		if ( values.isEmpty( ) )
 			return;
-		
+
 		IGetParameterDefinitionTask task = engine.createGetParameterDefinitionTask( report );
 		Collection params = task.getParameterDefns( false );
 		Iterator iter = values.keySet( ).iterator( );
 		while ( iter.hasNext( ) )
 		{
 			String name = (String) iter.next( );
-			
+
 			IScalarParameterDefn found = null;
 			Iterator i2 = params.iterator( );
 			while ( i2.hasNext( ) )
