e6c7f02471d24ec591a5db7aab51fc2a3fbb340d
jweathersby <jweathersby>
2005-10-15 07:15:50 +0000

    Examples Restructure

---
+++ examples/reports/hyperlinking/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Created on Feb 26, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+global $birtPath;
+$birtPath = "../../../";
+require $birtPath . "include/viewer.inc";
+viewer( "examples/reports/hyperlinking" );
+
+?>
