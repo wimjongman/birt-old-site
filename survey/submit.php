1d22161b1a8f476c270f39e11661d068cfa5b7a8
jweathersby <jweathersby>
2005-11-21 16:32:50 +0000

    update survey

--- survey/submit.php
+++ survey/submit.php
@@ -10,16 +10,63 @@
 // line.
 
 $line = "\"$$$\",\"" . date("Y-m-d G:i:s" ) . "\"";
-$fields = array( "LANGUAGE","DEV","IDE","REPORTWRITER","TOOLS","PRIMARY" );
+$fields = array( "LANGUAGE","DEV","IDE","SCRIPTTOOLS", "SCRIPTPRIMARY", "REPORTWRITER","TOOLS","PRIMARY", "INDUSTRY", "SOURCES" );
+$keys = array_keys($_POST);
+
+$script_tools = array("NONE", "JAVASCRIPT", "PERL", "PHP", "PYTHON", "RUBY", "OTHER");
+$tools        = array("BIRT", "CRYSTAL", "JASPER", "JSP", "JFREE", "JREPORTS", "REPORTNET", "STYLE", "OTHER");
+
+
 //echo print_r( $_POST );
+//print("<BR><BR>");
+//print_r($keys);
+
+$i = 0;
 foreach ( $fields as $field )
 {
-	$line .= ",\"" . $_POST[ $field ] . "\"";
+	$mod_line = "";
+
+	if($keys[$i] == "SCRIPTTOOLS") {
+		//print("<BR><BR>");
+		//print_r("Script Tools Key = " . $keys[$i] . " =====>> " . $_POST[$keys[$i]]);
+
+		foreach($script_tools as $script_tool) {		
+			if(strstr($_POST[$keys[$i]], $script_tool) == FALSE) {
+				$mod_line .= ",\"N\"";
+			} else {
+				$mod_line .= ",\"Y\"";
+			}
+		}
+		//print("<BR>mod_line = " . $mod_line . "<br>");
+		$line .= $mod_line;
+	} else if ($keys[$i] == "TOOLS") {
+		//print("<BR><BR>");
+		//print_r("Script Tools Key = " . $keys[$i] . " =====>> " . $_POST[$keys[$i]]);
+
+		foreach($tools as $tool) {		
+			if(strstr($_POST[$keys[$i]], $tool) == FALSE) {
+				$mod_line .= ",\"N\"";
+			} else {
+				$mod_line .= ",\"Y\"";
+			}
+		}
+		//print("<BR>mod_line = " . $mod_line . "<br>");
+		$line .= $mod_line;
+	} else {
+		$line .= ",\"" . $_POST[ $field ] . "\"";
+	}
+	
+	$i = $i + 1;
 }
+
+//print("<BR><BR>");
+
 $line .= "\n";
+
 //print "<br>\nLine: $line<br>\n";
 
 ignore_user_abort(true);
+
 $f = fopen( "../writable/results.csv", "a+t" );
 if ( ! $f )
 {
@@ -28,7 +75,7 @@ if ( ! $f )
 }
 flock( $f, LOCK_EX );
 
-// In case some other script has appended to the file while we were 
+// In case some other script has appended to the file while we were
 // waiting on flock()...
 
 fseek($f,0,SEEK_END);
@@ -58,8 +105,8 @@ ignore_user_abort(false);
 <img SRC="/birt/images/gradient.jpg" BORDER=0 height="53" width=282>
 </td>
 <td WIDTH="250" align="right" valign="center">
-<a href="/birt" class="birt">
-		    
+<a href="http://www.eclipse.org/birt" class="birt">
+
 <!-- Temporary BIRT header -->
 <span style="color: #E8E8FF; font-family: arial, sans-serif;
       font-size: 20px; font-weight: bold; padding-right: 20pt">
