6be70f03bbbd2917173f4e44d429be75f83888b2
jweathersby <jweathersby>
2006-01-10 21:21:54 +0000

    BPS hack temp fix

--- wiki/local/BPS.php
+++ wiki/local/BPS.php
@@ -1,6 +1,6 @@
 <?php if (!defined('PmWiki')) exit();
 
-include_once('cookbook/comments.php');
+##include_once('cookbook/comments.php');
 PatchCommentBrowseFmt();
 $EnableUpload = 1;
 $UploadMaxSize = 500000;
