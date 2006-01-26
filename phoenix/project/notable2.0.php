1b018d2a82a1617e3603541b165749eb66e16828
jweathersby <jweathersby>
2006-01-26 03:22:51 +0000

    phoenix beta

--- phoenix/project/notable2.0.php
+++ phoenix/project/notable2.0.php
@@ -23,6 +23,7 @@
 	$html = <<<EOHTML
 
 <div id="maincontent">
+	<div id="midcolumn">
 
 		<h1><a name="top"></a>$pageTitle</h1>
 		<blockquote>
@@ -215,7 +216,7 @@
 <p>The Data Set data types have been expanded to include Character Large Object (CLOB) and Binary Large Object (BLOB) items.  In addition to adding support for these types within the ODA framework, these items can now be bound to certain controls within the designer.  CLOB columns can be bound to Text controls and BLOB columns can be bound to Image controls.</p>
 <img src="/birt/phoenix/project/images/blobtype.JPG" width="603" height="457"/>
 <p>In the example above, the logo image is stored as a BLOB type.  The Image Builder has been enhanced to allow the BLOB image to be retrieved from the database and inserted into an Image control.</p>
-
+	</div>
 </div>
 
 
