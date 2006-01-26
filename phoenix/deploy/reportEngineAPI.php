950320fd23c01a5110feb37f4bd6ce5fe6a8fe4a
jweathersby <jweathersby>
2006-01-26 09:08:18 +0000

    phoenix beta

--- phoenix/deploy/reportEngineAPI.php
+++ phoenix/deploy/reportEngineAPI.php
@@ -127,6 +127,175 @@ shown in bold below.</p>
 &lt;/runtime&gt;
 </pre>
 
+  <p>Where <i>driver_name</i> is the name of your JDBC driver JAR. Add another <code>&lt;LibraryName></code> element for each 
+additional JDBC driver JAR file that might be needed. Put the library itself 
+  into the lib directory: <code><i>ENGINE-HOME</i>/plugins/org.eclipse.birt.report.data.oda.jdbc_version/lib.</code></p>
+  <p>More information about the ODA runtime driver extension point schema can be 
+  found in CVS BIRT source tree: <code>org.eclipse.birt.data.oda/schema/odaDriverRuntime.exsd</code>. 
+  Use the Eclipse PDE Tools to see its reference document.</p>
+
+<h2><a name="api"></a>API Overview</h2>
+
+<p>The following are the key steps to use the API, identifying the classes and 
+interfaces you use for each step.</p>
+
+<ol>
+  <li>Create an instance of <code>EngineConfig</code> to set options for the report engine.</li>
+  <li>Create an instance of the <code>ReportEngine</code> class. You can use this object to 
+  perform multiple tasks.</li>
+  <li>Open a report design using one of the <code>openReport(&nbsp;)</code> methods of <code>ReportEngine</code>.</li>
+  <li>Obtain information about report parameters using 
+  <code>IGetParameterDefinitionTask</code>.</li>
+  <li>Run and render a report using <code>IRunAndRenderReportTask</code>.</li>
+  <li>Repeat steps 3-5 for the next report.</li>
+  <li>When done, call <code>destroy(&nbsp;)</code> on your engine instance.</li>
+</ol>
+
+<p>The following sections describe the primary Engine classes in detail. The 
+Engine API includes a number of secondary helper classes referenced within each 
+description.</p>
+
+<h3><code>EngineConfig</code> Class</h3>
+
+<p>Use the <code>EngineConfig</code> class to set global options for the report engine as a 
+whole. Use it to specify the location of engine plugins, the location of data 
+drivers, and to add application-wide scriptable objects. At a minimum, you'll 
+want to set the location of the engine installation using the <code>setEngineHome(&nbsp;)</code> 
+method.</p>
+<pre class="code-block">EngineConfig config = new EngineConfig( );
+config.setEngineHome( &quot;put engine path here&quot; );</pre>
+The engine home should be set to
+installedlocation/birt-runtime-version/Report Engine
+
+
+<h3>Emitter Configuration</h3>
+
+<p>An &quot;emitter&quot; is the component of the report engine that produces output. BIRT provides two emitters: HTML and PDF. You'll want to configure the HTML 
+emitter to manage images and links. BIRT supports three kinds of images:</p>
+
+<ul>
+  <li>Images referenced using a URL</li>
+  <li>Embedded images</li>
+  <li>Images created to represent charts</li>
+</ul>
+
+<p>Your application must have a policy for handling images in HTML. URL-based 
+images are usually no problem. However, you'll need to handle the other two. The 
+<code>IHTMLImageHandler</code> interface defines the policy. The BIRT Engine provides two 
+default implementations:</p>
+
+<ul>
+  <li><code>HTMLCompleteImageHandler</code>: used to write images to disk when rendering a 
+  report to produce an HTML file on disk.</li>
+  <li><code>HTMLServerImageHandler</code>: used to handle images for an engine running in an 
+  app server. This class is used by the BIRT web app.</li>
+</ul>
+
+<p>You can also create your own implementation of <code>IHTMLImageHandler</code> if the above 
+don't meet your needs.</p>
+
+<p>Here's how to create an emitter configuration and set the image handler:</p>
+
+<pre class="code-block">EngineConfig config = new EngineConfig( );
+
+// Create the emitter configuration.
+
+HTMLEmitterConfig hc = new HTMLEmitterConfig( );
+
+// Use the &quot;HTML complete&quot; image handler to write the files to disk.
+
+HTMLCompleteImageHandler imageHandler = new HTMLCompleteImageHandler( );
+hc.setImageHandler( imageHandler );
+
+// Associate the configuration with the HTML output format.
+
+config.setEmitterConfiguration( HTMLRenderOption.OUTPUT_FORMAT_HTML, hc );</pre>
+
+
+<h3><code>ReportEngine</code> Class</h3>
+
+<p>The <code>ReportEngine</code>&nbsp; class represents the BIRT Report Engine. There is a significant cost associated with creating an engine instance, due 
+primarily to the cost of loading extensions. Therefore, each application should 
+create create just one <code>ReportEngine</code> instance and use it to run multiple reports.</p>
+<p>The report engine is created through its constructor that takes an 
+<code>EngineConfig</code> object as argument. The configuration object can be null, which 
+means to use the default engine configuration. After using the engine, call 
+<code>destroy(&nbsp;)</code> to do clean up work, which includes unloading the extensions.</p>
+
+<pre class="code-block">EngineConfig config = new EngineConfig( );
+...
+ReportEngine engine = new ReportEngine( config );
+
+// Run reports, etc.
+...
+
+// Shut down the engine.
+
+try
+{
+    engine.destroy( );
+}
+catch ( EngineException e1 )
+{
+    // Ignore
+}</pre>
+
+
+<h3><code>IReportRunnable</code> Class</h3>
+
+
+<p>BIRT reports are stored as an XML file. To work with the report in the 
+engine, you must first load the report using one of the <code>openDesign(&nbsp;)</code> methods in 
+the <code>ReportEngine</code> class. These methods return an 
+<code>IReportRunnable</code> instance that represents the engine's view of the 
+report design. </p>
+
+<pre class="code-block">IReportRunnable report = engine.openReportDesign( name );</pre>
+
+<p>Use this object to obtain parameter data or run the report. The class 
+provides methods for getting report properties such as the title, author and so 
+on. It also provides methods for getting images embedded (stored) within the 
+report design. If your application requires more information about the design, you can 
+obtain a Design Engine report handle, then use the BIRT <a href="de-api.html">
+Design Engine API</a> to traverse though the report design.</p>
+
+<h3><code>IEngineTask</code> Abstract Base Class</h3>
+
+<p>BIRT reports support scripting. Operations that execute scripts require a 
+scripting context. Report operations also require a locale. BIRT Engine tasks 
+provide the framework for managing the scripting context, report locales and so 
+on. In general, if an operation requires neither a script context nor a locale, 
+it will appear as a method on the engine or the report design. However, if the 
+operation does 
+require these items; then the operation is represented by a task class.</p>
+
+<p>For example, opening a design file or retrieving an image in the design file 
+do not require setting up a scripting context. Other operations, such as 
+retrieving default parameters, retrieving a dynamic selection list, and running 
+and rendering a report, all support scripting, require a scripting context, and 
+are represented as tasks.</p>
+
+<p>Create tasks using the factory methods on the <code>ReportEngine</code> class. Each task 
+takes a report runnable (design), a scripting context, a locale, and other 
+information.</p>
+
+<h3><code>IGetParameterDefinitionTask</code> Class</h3>
+
+<p>Use this task to obtain information about parameters. Parameter default 
+values are expressions, and so a scripting context (represented by the task) is 
+required. Parameter definitions provide access to the parameter 
+definition information that the report designer entered at design time. If a 
+parameter has custom XML or user-defined properties defined, then these are also 
+available. Applications typically use the standard and custom properties to 
+create a parameter UI that prompts the user for parameter values. Parameters can 
+be organized into groups. Your application has the choice of retrieving the 
+parameters organized by group (as they should be displayed to the user), or in 
+ungrouped form (useful for creating a programmatic interface.)</p>
+
+<p>The <code>IParameterGroupDefn</code> and <code>IScalarParameterDefn</code>
+interfaces provide 
+information about parameter groups and individual parameters.</p>
+
 
 	</div>
 	$deployLinksSideItem
