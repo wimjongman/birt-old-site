f43f1e11c68c5cbf24962165d9fbf550dd89ca06
jweathersby <jweathersby>
2006-01-26 00:49:22 +0000

    phoenix beta

--- phoenix/_projectCommon.php
+++ phoenix/_projectCommon.php
@@ -15,19 +15,20 @@
 		$theme = $App->getUserPreferedTheme();
 	}
 
-    $Nav->addCustomNav("Introduction", "/birt/intro", "_self", 1);
+    $Nav->addCustomNav("BIRT Home", "/birt/phoenix", "_self", 1);
+    $Nav->addCustomNav("Introduction", "/birt/phoenix/intro", "_self", 1);
 	$Nav->addCustomNav("Download", 			"http://download.eclipse.org/birt/downloads", 	"_self", 	1);
-	$Nav->addCustomNav("Install", 			"/birt/build/", 	"_self", 							1);
-	$Nav->addCustomNav("Tutorial", 			"/birt/tutorial", 	"_self", 									1);
-	$Nav->addCustomNav("Sample Database", 		"/birt/db", 	"_self", 										1);
-	$Nav->addCustomNav("Examples", 			"/birt/examples", 	"_self", 									1);
-	$Nav->addCustomNav("Integration", 		"/birt/deploy", 	"_self", 										1);
-	$Nav->addCustomNav("FAQ", 			"/wiki/index.php?n=FAQ.FAQ", 	"_self", 						1);
-	$Nav->addCustomNav("Community", 		"/birt/community", 	"_self", 								1);
-	$Nav->addCustomNav("Reference", 		"/birt/ref", 	"_self", 											1);
-	$Nav->addCustomNav("Project Organization", 	"/birt/project", 	"_self", 									1);
-	$Nav->addCustomNav("Project Development Plan", 	"/birt/project/plan.php", 	"_self", 	1);
-	$Nav->addCustomNav("Building BIRT", 		"/birt/build", 	"_self", 									1);
-	$Nav->addCustomNav("Search", 			"/birt/search.html", 	"_self", 									1);
+	$Nav->addCustomNav("Install", 			"/birt/phoenix/build/", 	"_self", 							1);
+	$Nav->addCustomNav("Tutorial", 			"/birt/phoenix/tutorial", 	"_self", 									1);
+	$Nav->addCustomNav("Sample Database", 		"/birt/phoenix/db", 	"_self", 										1);
+	$Nav->addCustomNav("Examples", 			"/birt/phoenix/examples", 	"_self", 									1);
+	$Nav->addCustomNav("Integration", 		"/birt/phoenix/deploy", 	"_self", 										1);
+	#$Nav->addCustomNav("FAQ", 			"/wiki/index.php?n=FAQ.FAQ", 	"_self", 						1);
+	$Nav->addCustomNav("Community", 		"/birt/phoenix/community", 	"_self", 								1);
+	$Nav->addCustomNav("Reference", 		"/birt/phoenix/ref", 	"_self", 											1);
+	$Nav->addCustomNav("Project Organization", 	"/birt/phoenix/project", 	"_self", 									1);
+	$Nav->addCustomNav("Project Development Plan", 	"/birt/phoenix/project/plan.php", 	"_self", 	1);
+	$Nav->addCustomNav("Building BIRT", 		"/birt/phoenix/build", 	"_self", 									1);
+	#$Nav->addCustomNav("Search", 			"/birt/search.html", 	"_self", 									1);
 
 ?>
