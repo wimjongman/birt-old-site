53dcc424e3f56a5f6ca5d7b2640491e3b46a05b8
jweathersby <jweathersby>
2006-02-08 23:51:41 +0000

    *** empty log message ***

---
+++ phoenix/wiki/local/Release20Specs.php
@@ -0,0 +1,9 @@
+<?php if (!defined('PmWiki')) exit();
+
+include_once('cookbook/comments.php');
+PatchCommentBrowseFmt();
+$EnableUpload = 1;
+$UploadMaxSize = 500000;
+$UploadDir = "/home/data/httpd/writable/birt/wiki.d/uploads";
+$UploadUrlFmt = "http://www.eclipse.org/birt/wiki/wiki.d/uploads";
+
