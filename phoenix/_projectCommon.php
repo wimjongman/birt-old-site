48bed476c953978b1aac0877215179024f8f145f
jweathersby <jweathersby>
2006-09-01 17:00:52 +0000

    Added link to the wiki

--- phoenix/_projectCommon.php
+++ phoenix/_projectCommon.php
@@ -23,7 +23,7 @@
 	$Nav->addCustomNav("Sample Database", 		"/birt/phoenix/db", 	"_self", 										1);
 	$Nav->addCustomNav("Examples", 			"/birt/phoenix/examples", 	"_self", 									1);
 	$Nav->addCustomNav("Integration", 		"/birt/phoenix/deploy", 	"_self", 										1);
-	#$Nav->addCustomNav("FAQ", 			"/wiki/index.php?n=FAQ.FAQ", 	"_self", 						1);
+	$Nav->addCustomNav("FAQ/Wiki", 			"http://wiki.eclipse.org/index.php/BIRT_Project", 	"_self", 						1);
 	$Nav->addCustomNav("Community", 		"/birt/phoenix/community.php", 	"_self", 								1);
 	$Nav->addCustomNav("Reference", 		"/birt/phoenix/ref", 	"_self", 											1);
 	$Nav->addCustomNav("Project Organization", 	"/birt/phoenix/project", 	"_self", 									1);
