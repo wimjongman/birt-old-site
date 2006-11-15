e9f1ec89cdf7ede80d5b3ab1638a6b9e9c4da790
jweathersby <jweathersby>
2006-11-15 08:58:52 +0000

    main page mods

--- phoenix/chk.php
+++ phoenix/chk.php
@@ -14,17 +14,42 @@ function checkLink($link=0)
 	}
 }
 
+   function url_exists($url) {
+       $a_url = parse_url($url);
+       if (!isset($a_url['port'])) $a_url['port'] = 80;
+       $errno = 0;
+       $errstr = '';
+       $timeout = 30;
+       if(isset($a_url['host']) && $a_url['host']!=gethostbyname($a_url['host'])){
+           $fid = fsockopen($a_url['host'], $a_url['port'], $errno, $errstr, $timeout);
+           if (!$fid) return false;
+           $page = isset($a_url['path'])  ?$a_url['path']:'';
+           $page .= isset($a_url['query'])?'?'.$a_url['query']:'';
+           fputs($fid, 'HEAD '.$page.' HTTP/1.0'."\r\n".'Host: '.$a_url['host']."\r\n\r\n");
+           $head = fread($fid, 4096);
+           fclose($fid);
+           return preg_match('#^HTTP/.*\s+[200|302]+\s#i', $head);
+       } else {
+           return false;
+       }
+   }
+
+
+
 //$install_instructions_version = "http://www.eclipse.org/birt/phoenix/build/" . "install_" . "R2_1_1" . ".php";
 $install_instructions_version = "http://www.google.com";
 
-if ( checkLink( $install_instructions_version ) ){
+if ( url_exists( $install_instructions_version ) ){
 ?>
 	Found it!!!!
 <?php 
 }else{ 
 ?>
     Did not find it!!!!
-<?php 
+
+<?php
+print_r(get_headers($install_instructions_version));
+    
 }
 
 
