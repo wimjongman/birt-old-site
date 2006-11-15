36389a0867922c25209bcf1edf5911e561d1c711
jweathersby <jweathersby>
2006-11-15 08:40:43 +0000

    main page mods

--- phoenix/chk.php
+++ phoenix/chk.php
@@ -13,25 +13,11 @@ function checkLink($link=0)
 		return FALSE;
 	}
 }
-function chklnk($link=0)
-{
 
-
-$ch = curl_init("http://www.google.com"); /// initialize a cURL session 
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-$xmlResponse = curl_exec ($ch);
-curl_close ($ch);
-
-if (!is_string($xmlResponse) || !strlen($xmlResponse)) {
-   return false;
-} else {
-   return true;
-}
-}
 //$install_instructions_version = "http://www.eclipse.org/birt/phoenix/build/" . "install_" . "R2_1_1" . ".php";
 $install_instructions_version = "http://www.google.com";
 
-if ( chklnk( $install_instructions_version ) ){
+if ( checkLink( $install_instructions_version ) ){
 ?>
 	Found it!!!!
 <?php 
