866298edff89d77d6ab8c95a77c9c2860c9fbe3e
jweathersby <jweathersby>
2006-11-15 08:20:13 +0000

    main page mods

---
+++ phoenix/chk.php
@@ -0,0 +1,27 @@
+<?php
+
+function checkLink($link=0)
+{
+
+	$fp = @fopen($link,"r");
+	if ($fp)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+$install_instructions_version = "http://www.eclipse.org/birt/phoenix/build/" . "install_" . "R2_1_1" . ".php";
+if ( checkLink( $install_instructions_version ) ){
+?>
+	Found it
+<?php }else{ ?>
+    Did not find it
+ <?php}
+ ?>
+
+	
+	
+?>
