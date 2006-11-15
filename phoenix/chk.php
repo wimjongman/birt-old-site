60e3dfafcf4f0fc8b2edd929bb8476c9beff8a77
jweathersby <jweathersby>
2006-11-15 09:26:30 +0000

    main page mods

--- phoenix/chk.php
+++ phoenix/chk.php
@@ -4,14 +4,14 @@
 This is a test
 <?php
 
-$fp = fopen('http://www.google.com', 'r');
-
+//$fp = fopen('http://www.google.com', 'r');
+$ggg = get_meta_tags('http://www.google.com/');
 // the variable $http_response_header magically appears
 $aaa = "Test1";
 $bbb = "Test2";
 
 print_r($aaa);
-print_r($http_response_header);
+print_r($ggg);
 print_r($bbb);
 
 
