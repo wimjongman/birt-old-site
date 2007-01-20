1bde53599f8bc25552f897259d640068419b39c7
jweathersby <jweathersby>
2007-01-20 03:10:14 +0000

    wiki link update

---
+++ wiki/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Created on Feb 25, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+//require "include/viewer.inc";
+//viewer( $_GET['page'] );
+Header("Location: http://wiki.eclipse.org/index.php/BIRT_Project")
+
+?>
