321cf49760b7867bf23cb0518622dfdf3f0655c7
jweathersby <jweathersby>
2008-06-10 18:15:42 +0000

    moved project plan page to wiki

--- phoenix/project/plan.php
+++ phoenix/project/plan.php
@@ -1,108 +1,13 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php
+/*
+ * Created on Feb 25, 2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ 
+//require "include/viewer.inc";
+//viewer( $_GET['page'] );
+Header("Location: http://wiki.eclipse.org/BIRT_Project_Plan")
 
-	#
-	# Begin: page-specific settings.  Change these.
-	$pageTitle 		= "Business Intelligence and Reporting Tools (BIRT) Development Plans";
-	$pageKeywords	= "";
-	$pageAuthor		= "";
-
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
-	# End: page-specific settings
-	#
-
-	require "../birt.inc";
-	$linkToTop				= linkToTop();
-
-
-	# Paste your HTML content between the EOHTML markers!
-	$html = <<<EOHTML
-
-<div id="maincontent">
-	<div id="midcolumn">
-
-		<h1><a name="top"></a>$pageTitle</h1>
-
-		<h2><a name="Overview"></a>Overview</h2>
-		<p>
-			Welcome to the Business Intelligence and Reporting Tools (BIRT) Project pages. This page contains links to Project Plans for the version(s) of BIRT currently in Development, as well as archives of previous release plans.
-		</p>
-		
-		<h2><a name="latest">Latest Release(s) Available </a></h2>
-		<p>
-		BIRT Release 2.1.3 (July 2007) is available for <a href=http://download.eclipse.org/birt/downloads/index2.1.3.php>download</a>. For information on features in this release, see the <a href="/birt/phoenix/project/notable2.1.php">BIRT 2.1.2 New and Notable</a> page.<br>
-		BIRT Release 2.2.1.1 (Oct 2007) is available for <a href=http://download.eclipse.org/birt/downloads/>download</a>. For information on features in this release, see the <a href="/birt/phoenix/project/notable2.2.php">BIRT 2.2 New and Notable</a> page.
-		</p>
-
-		<h2>Releases Under Development/Planned</h2>
-
-
-
-
-		<h3><i>BIRT Release 2.3</i></h3>
-		Target Date: TBD
-		
-		<p>
-
-
-				 
-				<a
-				href="
-https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=BIRT&product=BIRT&target_milestone=2.3.0&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_severity=blocker&bug_severity=critical&bug_severity=major&bug_severity=normal&bug_severity=minor&bug_severity=trivial&bug_severity=enhancement&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0="
-				>BIRT 2.3 Enhancements and Bug Fixes </a>
-
-		<br>
-		<a href="/birt/phoenix/project/project_plan_R2_3_0.php">BIRT 2.3 Project Development Plan</a>
-		</p>	
-
-
-
-
-
-
-
-		
-		<h3><i>BIRT Release 2.2.2</i></h3>
-		Target Date: January, 2007
-<p>
-
-
-
-				<a
-				href="
-https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=BIRT&product=BIRT&target_milestone=2.2.2&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_severity=blocker&bug_severity=critical&bug_severity=major&bug_severity=normal&bug_severity=minor&bug_severity=trivial&bug_severity=enhancement&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=
-				"
-				>BIRT 2.2.2 Enhancements and Bug Fixes </a>
-
-	
-<br>
-		<a href="/birt/phoenix/project/project_plan_R2_2_2.php">BIRT 2.2.2 Project Development Plan</a>
-		</p>	
-		
-
-
-		
-		<h2><a name="archive"></a>Archive</h2>
-		<p>
-			This section contains links to information used in the development process for prior versions of BIRT.
-		</p>
-		<ul class="midlist">
-			<li>
-				<a href="/birt/phoenix/project/archived.php">Archived Development Plans</a> – 1.x and 2.x Releases
-			</li>
-		</ul>
-		
-	</div>
-</div>
-
-
-EOHTML;
-
-
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
