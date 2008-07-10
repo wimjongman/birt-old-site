09d693f7f5d717ba6004570e6d13de196d635ab8
jweathersby <jweathersby>
2008-07-10 00:13:36 +0000

    install instructions

--- phoenix/build/index.php
+++ phoenix/build/index.php
@@ -25,15 +25,8 @@
 
 		<blockquote>
 			<ul>
-				<li><a href="#introduction">Introduction</a></li>
-				<li><a href="#allinone">Report Designer Full Eclipse Install</a></li>
-				<li><a href="#designer">Designer Install</a></li>
-				<li><a href="#gef">GEF</a></li>
-				<li><a href="#emf">EMF</a></li>
-				<li><a href="#dtp">DTP</a></li>
-				<li><a href="#birt">BIRT</a></li>
-				<li><a href="#optional">Optional Eclipse Link File</a></li>
-				<li><a href="#itext">iText</a></li>
+				<li><a href="#introduction">Designer Install</a></li>
+				<li><a href="#designer">Framework Designer Install</a></li>
 				<li><a href="#drivers">JDBC Drivers</a></li>
 				<li><a href="#updating">Updating a BIRT Installation</a></li>
 				<li><a href="#j2ee">Deploying to J2EE Server</a></li>
@@ -45,64 +38,29 @@
 
 
 
-<h2><a name="introduction"></a>Introduction</h2>
+<h2><a name="introduction"></a>Designer Install</h2>
 <p>
-Below are complete instructions for installing BIRT. If you are already
-familiar with Eclipse plugins, you might want to follow the abbreviated
-requirements and instructions on the
-<a href="http://download.eclipse.org/birt/downloads">
-download page</a>.
-Otherwise, follow along below for detailed steps for installing BIRT.
-<p>
-The instructions here are for the Release 2.2/2.1.3 (and later) builds.
-<h2>Java</h2>
-<p>
- You'll need a <a href="http://java.sun.com">Java runntime environment (JRE)</a>
- The version you choose will depend on the version of Eclipse you are running.
- See below. 
-<h2>Eclipse</h2>
-<p>
-BIRT is currently tested with the following configurations:<br>
+BIRT can be downloaded and installed in different ways.<br>
+
 <ul>
-<li><b>BIRT 2.1.3</b>
-<li>Eclipse 3.2.
-<li>GEF 3.2.
-<li>EMF 2.2.
-<li>JRE 1.4.2/JRE 1.5.
+<li>The complete BIRT designer or RCP Designer can be installed by downloading the required package <a href="http://download.eclipse.org/birt/downloads">here</a>.
+</li>
+<li>or by downloading the <a href="http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/ganymede/R/eclipse-reporting-ganymede-win32.zip">Ganymede Eclipse IDE for Java and Report Developers Package</a>   
+</li>
+<li>or through the update manager as described <a href="http://download.eclipse.org/birt/downloads/updmaninst2.2.php">here</a>.
+</li>
+<li>as a package that only contains the BIRT plugins (Framework) from the <a href="http://download.eclipse.org/birt/downloads">download page</a>.  Note additional requirements are located on the download page as well.  These
+include dependencies on other Eclipse projects.  These dependencies are listed on the download page for the specific build.
 </ul>
-And
-<ul>
-<li><b>BIRT 2.2</b>
-<li>Eclipse 3.3.
-<li>GEF 3.3.
-<li>EMF 2.3.
-<li>JRE 1.5.
-<li>DTP 1.5.
-</ul>
-
-
-<h2><a name="allinone"></a>Report Designer Full Eclipse Install</h2>
-BIRT now supplies a <a href="http://download.eclipse.org/birt/downloads/">download</a> that incudes Eclipse, GEF, EMF and BIRT.  Use this
-download if you do not already have Eclipse installed. 
-
-<br>
-<h2><a name="designer"></a>Designer Install</h2>
-Use these instructions if you have Eclipse installed or do not desire to use the
-Report Designer Full Eclipse Install.
-<br>
-Download and install the
-<a href="http://download.eclipse.org/eclipse/downloads/">
-Eclipse Platform 3.1/3.2/3.3 Release</a>
 <p>
-(Note: Eclipse, EMF, GEF, DTP and BIRT are all available from the
-<a href="http://www.eclipse.org/downloads/index.php">Eclipse download page</a>.)
+All of the above downloads contain the BIRT designer.  The BIRT project has many additional downloads, including a runtime for deploying the BIRT engine in a web application or standalone Java application.
+The additional downloads are available for any build of BIRT by selecting the appropriate build <a href="http://download.eclipse.org/birt/downloads/build_list.php">here.</a>
+</p>
 <p>
-Eclipse comes as a zip file or tar file. On Windows, unzip the install file
-into your "C:\Program Files" directory. This will create a subdirectory called eclipse.
-
-<p>
-To make it easier to start Eclipse on Windows, create a shortcut to Eclipse on your desktop:
-
+Once you have downloaded one of the above packages, unzip the package into your chosen directory.  This will create an eclipse directory that
+contains eclipse.exe.  Run eclipse.exe to start the designer.
+</p>
+<h3>Create a windows short cut</h3>
 <ul>
 <li>From the desktop, right click, choose New&rarr;Shortcut.
 <li>For location, enter "C:\Program Files\eclipse\eclipse.exe" (without quotes.)
@@ -110,90 +68,23 @@ To make it easier to start Eclipse on Windows, create a shortcut to Eclipse on y
 <li>Enter “Eclipse” for the shortcut name.
 </ul>
 
-<h2><a name="gef"></a>GEF</h2>
-<p>
-GEF, the Graphic Editor Framework, is an Eclipse plugin used by the BIRT UI.
-Download the
-<a href="/birt/frameizer.php?page=http://download.eclipse.org/tools/gef/downloads/index.php">
-GEF 3.1/3.2/3.3 Release, GEF Runtime</a>.
-<p>
-GEF comes as a zip file. Unzip GEF into directory that contains Eclipse. If you
-installed Eclipse into the standard location on Windows, then unzip GEF into
-"C:\Program Files".
 
-<h2><a name="emf"></a>EMF</h2>
 
+<h2><a name="designer"></a>Framework Designer Install</h2>
 <p>
-The BIRT charting feature uses EMF, the Eclipse Modeling Framework. Download the
-<a href="http://www.eclipse.org/modeling/emf/downloads/">
-EMF & SDO RT version 2.1/2.2/2.3</a>. Look in the Latest Releases sections of the
-download page.
-<p>
-EMF also comes as a zip file. Unzip EMF into directory that contains Eclipse.
-This is the same directory you used for GEF.
-
-<h2><a name="dtp"></a>DTP</h2>
-
-<p>
-BIRT sources data for reports using the Data Tools Platform Project. Download 
-<a href="http://www.eclipse.org/datatools/downloads.html">
-DTP version 1.5</a>. Note that 1.5 is only a requirement for versions of BIRT newer than
-BIRT 2.2 M6.  2.2 M6 is the last version of BIRT to include the required DTP plugins in the designer download.
-</p><p>  
-The BIRT allinone download will continue to include the required DTP plugins.  Additionally a separate download is now
-available on the BIRT site that is a subset of the DTP plugins.  This package referred to as the BIRT/DTP Integration package
-contains only the required DTP Plugins needed for BIRT to function. 
+As stated above, the framework install only contains the required BIRT plugins.
+The Framework install requires that you already have the Eclipse Platform, GEF, EMF, DTP and WTP packages.  The specific version of
+each, is listed on the specific build's download page. Unzip each package into your eclipse directory.  
 </p>
-
-
-
-<h2><a name="birt"></a>BIRT</h2>
-
-<p>
-Download <a href="http://download.eclipse.org/birt/downloads/index2.1.3.php">
-Release 2.1.3</a> (or <a href="http://download.eclipse.org/birt/downloads/">later</a>). Download only the
-BIRT designer framework for now: "birt-report-framework-2_1_3.zip".
-<p>
-BIRT is also a zip file. Unzip BIRT into the directory that contains Eclipse.
-
 <h2><a name="optional"></a>Optional Eclipse Link File</h2>
-<p>If you currently have an Eclipse install with GEF and EMF, you can download the
+<p>If you are installing the framework, you can download the
 BIRT framework and unzip the contents to any directory.
 <ul>
-<li>After doing this, create a links directory in your installed Eclipse location (ie C:\eclipse3.2\eclipse\links).
-<li>Within this directory create a file with a unique name like birt.2.1.link.
-<li>Open the file you just created and enter path=drive:/pathtobirt (ie path=c:/birt2.1)
-<li>You will still need iText and prototype.js files.  See next section. 
+<li>After doing this, create a links directory in your installed Eclipse location (ie C:\eclipse\links).
+<li>Within this directory create a file with a unique name like birt.2.3.link.
+<li>Open the file you just created and enter path=drive:/pathtobirt (ie path=c:/birt2.3)
 </ul>
-
-<h2><a name="itext"></a>iText</h2>
-<ul>
-<li><a href="http://prdownloads.sourceforge.net/itext/itext-1.3.jar">iText 1.3</a>.
-</ul>
-<p>
-The iText APIs are discussed at <a href="http://www.lowagie.com/iText/">iText Homepage</a><br><br> 
-If you are installing BIRT 2.1.3/2.2, this file is already included. <br><br>
-<br><b>BIRT 2.1.2</b><br>
-The itext-1.3.jar file needs to be copied to the plugins/com.lowagie.itext_*/lib directory.
-
-<br>
-<div class="homeitem3col">
-<h3>Birt 2.1.2 (This file is included in the 2.1.3/2.2 release): </h3>
-<ul>
- To Support Asian fonts in PDF, download the following jar and place it in the plugins/com.lowagie.itext_*/lib directory.
-<li><a href="http://prdownloads.sourceforge.net/itext/iTextAsian.jar">iTextAsian.jar</a>.
-
- </ul>
-</div>
-
-<br>
 </p>
-
-
-<br>
-
-
-
 
 <h2><a name="drivers"></a>JDBC Drivers</h2>
 
@@ -219,22 +110,11 @@ default location, this is "C:\program files\eclipse\plugins".
 <li>Delete all directories that start with "org.eclipse.birt".
 <li>Download and install BIRT as described in the BIRT section above.
 <li>Restart Eclipse with the -clean option: <code>eclipse -clean</code></ul>
-
+</ul>
 
 
 <h2><a name="j2ee"></a>Deploying to J2EE Server</h2>
 Please see <a href="/birt/phoenix/deploy/viewerSetup.php">Setting up the BIRT Viewer</a>
-<br>
-<div class="homeitem3col">
-<h3>Birt 2.1.2 Runtime Note (Not needed for BIRT 2.1.3/2.2): </h3>
-<ul>
- <li>If you are installing the 2.1.2 Runtime (J2EE Application Server Deployment),
- <li>Copy <a href="http://prdownloads.sourceforge.net/itext/itext-1.3.jar">itext-1.3.jar</a> to /WebViewerExample/WEB-INF/platform/plugins/com.lowagie.itext/lib.
- <li>Copy <a href="http://prdownloads.sourceforge.net/itext/iTextAsian.jar">iTextAsian.jar</a> to /WebViewerExample/WEB-INF/platform/plugins/com.lowagie.itext/lib.
-
- </ul>
-</div>
-<br>
 <br>
 
 
