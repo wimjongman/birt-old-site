9f41499000144b97fe9e35eb1fe40a1c5134a96e
jweathersby <jweathersby>
2011-06-21 16:30:18 +0000

    BIRT 3.7 enhancements

--- phoenix/intro/intro05.php
+++ phoenix/intro/intro05.php
@@ -65,7 +65,7 @@
 		</p>
 		<h2>Output Formats</h2>
 		<p>
-			BIRT provides output in HTML, Paginated HTML, PDF, XLS, DOC, PPT, and Postscript. Many other types of output
+			BIRT provides output in HTML, Paginated HTML, PDF, XLS, DOC, PPT, ODS, ODP, ODT, and Postscript. Many other types of output
 			are possible: ERich Text Format (RTF), Scalable Vector Graphic (SVG),
 			images, and more. While BIRT will add some of these over time, others may
 			have a more limited audience. Developers can use BIRT engine interfaces to
