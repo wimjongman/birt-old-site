96e744b81ec8dca8d2064ee25bf7662d718cfa41
mwilliams <mwilliams@mwi06431.actuate.com>
2013-06-26 15:40:18 -0500

    Change second button on main page point to 3.7.2

--- phoenix/index.php
+++ phoenix/index.php
@@ -59,10 +59,10 @@
 	</tr>	
 	<tr>
 	<td>
-	<a href="http://download.eclipse.org/birt/downloads/index4.2.2.php"><img src="DB4-2-2.jpg"></a>
+	<a href="http://download.eclipse.org/birt/downloads/index3.7.2.php"><img src="DB3-7-2.jpg"></a>
 	</td>
 	<td>
-	Get started with the latest in the BIRT 4.2.2 series.
+	Get started with the latest in the BIRT 3.7 series.
 	</td>
 	</tr>
 
