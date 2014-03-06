132944acb16490dc70e79735fe48d9a836afb249
mwilliams1120 <mwilliams@actuate.com>
2014-03-06 13:42:38 -0600

    Page update

--- phoenix/index.php
+++ phoenix/index.php
@@ -5,6 +5,17 @@
 	$App 	= new App();
 	$Nav	= new Nav();
 	$Menu 	= new Menu();
+	
+	<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PW8LFV"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PW8LFV');</script>
+<!-- End Google Tag Manager -->
+	
 	include($App->getProjectCommon());
 
 	#*****************************************************************************
@@ -38,15 +49,7 @@
 	$html = <<<EOHTML
 
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-PW8LFV"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PW8LFV');</script>
-<!-- End Google Tag Manager -->
+
 
 
 
