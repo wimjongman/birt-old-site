cdfb06eba681f449c1d134743b47e3b7a4fb2857
mwilliams1120 <mwilliams@actuate.com>
2014-03-12 15:41:08 -0500

    testing

--- test/index.php
+++ test/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once( $_SERVER['DOCUMENT_ROOT'] . "birt/common/config.php");
+require_once( $_SERVER['DOCUMENT_ROOT'] . "birt/test/common/config.php");
 $PagePart 	= new PagePart();
 
 ?>
