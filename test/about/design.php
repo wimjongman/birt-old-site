c917368f4850b06e45c543f7ec61abfc821befde
mwilliams1120 <mwilliams@actuate.com>
2014-04-10 12:21:19 -0500

    site facelift

--- test/about/design.php
+++ test/about/design.php
@@ -69,98 +69,18 @@ $PagePart 	= new PagePart();
 
 			        </div>
 
-			        <a name="designerinstall"></a>
-					<h2 class="topic-section-header padding-top-small">Designer Install</h2>
-					<p>BIRT can be downloaded and installed in different ways.
+			        <h2 class="topic-section-header padding-top-small">Report Introduction</h2>
+					<p>BIRT is an Eclipse-based open source reporting system for web applications, especially those based on Java and Java EE. BIRT has two main components: a report designer based on Eclipse, and a runtime component that you can add to your app server. BIRT also offers a charting engine that lets you add charts to your own application.</p>
+					<p>We encourage you to download BIRT, try it, and give us your feedback though the <a href="/birt/test/community/">forums, newsgroup and Bugzilla</a>.</p>
+					<p>With BIRT, you can add a rich variety of reports to your application.
 					<ul class="eclipse-list">
-						<li>The complete BIRT designer or RCP Designer can be installed by downloading the required package here.</li>
-						<li>or by downloading the Eclipse IDE for Java and Report Developers Package</li>
-						<li>or through the update manager as described here. Make sure to use the latest Update Site.<br />Update Manager URLs </li>
-						<li>as a package that only contains the BIRT plugins (Framework) from the download page. Note additional requirements are located on the download page as well. These include dependencies on other Eclipse projects. These dependencies are listed on the download page for the specific build.</li>
+						<li>Lists - The simplest reports are lists of data. As the lists get longer, you can add grouping to organize related data together (orders grouped by customer, products grouped by supplier). If your data is numeric, you can easily add totals, averages and other summaries.</li>
+						<li>Charts - Numeric data is much easier to understand when presented as a chart. BIRT provides pie charts, line & bar charts and many more. BIRT charts can be rendered in SVG and support events to allow user interaction.</li>
+						<li>Crosstabs - Crosstabs (also called a cross-tabulation or matrix) shows data in two dimensions: sales per quarter or hits per web page.</li>
+						<li>Letters & Documents - Notices, form letters, and other textual documents are easy to create with BIRT. Documents can include text, formatting, lists, charts and more.</li>
+						<li>Compound Reports - Many reports need to combine the above into a single document. For example, a customer statement may list the information for the customer, provide text about current promotions, and provide side-by-side lists of payments and charges. A financial report may include disclaimers, charts, tables all with extensive formatting that matches corporate color schemes.</li>
 					</ul>
 					</p>
-					<p>All of the above downloads contain the BIRT designer. The BIRT project has many additional downloads, including a runtime for deploying the BIRT engine in a web application or standalone Java application. The additional downloads are available for any build of BIRT by selecting the appropriate build here.</p>
-					<p>Once you have downloaded one of the above packages, unzip the package into your chosen directory. This will create an eclipse directory that contains eclipse.exe. Run eclipse.exe to start the designer.</p>
-					
-					<h2 class="topic-section-header padding-top-small">Create a windows short cut</h2>
-					<ul class="eclipse-list">
-						<li>From the desktop, right click, choose New→Shortcut.</li>
-						<li>For location, enter "C:\Program Files\eclipse\eclipse.exe" (without quotes.)</li>
-						<li>Click Next.</li>
-						<li>Enter "Eclipse" for the shortcut name.</li>
-					</ul>
-					
-					<a name="frameworkinstall"></a>
-					<h2 class="topic-section-header padding-top-small">Framework Designer Install</h2>
-					<p>As stated above, the framework install only contains the required BIRT plugins. The Framework install requires that you already have the Eclipse Platform, GEF, EMF, DTP and WTP packages. The specific version of each, is listed on the specific build's download page. Unzip each package into your eclipse directory.</p>
-
-					<h2 class="topic-section-header padding-top-small">Optional Eclipse Link File</h2>
-					<p>If you are installing the framework, you can download the BIRT framework and unzip the contents to any directory.</p>
-					<ul class="eclipse-list">
-						<li>After doing this, create a links directory in your installed Eclipse location (ie C:\eclipse\links).</li>
-						<li>Within this directory create a file with a unique name like birt.link.</li>
-						<li>Open the file you just created and enter path=drive:/pathtobirt (ie path=c:/birt)</li>
-					</ul>
-
-					<a name="jdbcdriver"></a>
-					<h2 class="topic-section-header padding-top-small">JDBC Drivers</h2>
-					<p>You will likely want to use your own JDBC driver with BIRT. Use the data source editor's JDBC driver management wizard. To start the wizard, open a BIRT report design, go to the Data Explorer view, right click on "Data Sources" and select "New Data Source". Choose "JDBC Data Source" and click "Next". In the next dialog, choose "Manage Drivers..." to bring up the "Manage JDBC Drivers" dialog.</p>
-					<p>In the "JAR Files" tab, click on "Add..." to add the JAR file required by your JDBC driver. Then go to the "Driver" tab to confirm that the list of drivers includes the new drivers added. You may also want to assign a display name and URL template for the new drivers in this tab.</p>
-
-					<a name="updatebirtinstall"></a>
-					<h2 class="topic-section-header padding-top-small">Updating a BIRT Installation</h2>
-					<p>If you have a version of BIRT installed, and want to install a newer version, do the following:</p>
-					<ul class="eclipse-list">
-						<li>Close Eclipse.</li>
-						<li>Locate your Eclipse plugins directory. If you installed on Windows in the default location, this is "C:\program files\eclipse\plugins".</li>
-						<li>Delete all directories that start with "org.eclipse.birt".</li>
-						<li>Download and install BIRT as described in the BIRT section above.</li>
-						<li>Restart Eclipse with the -clean option: eclipse -clean</li>
-					</ul>
-
-					<a name="deploytojava"></a>
-					<h2 class="topic-section-header padding-top-small">Deploying to Java EE Server</h2>
-					<p>Please see Setting up the BIRT Viewer </p>
-
-					<a name="birtlanguage"></a>
-					<h2 class="topic-section-header padding-top-small">Installing BIRT Language Packs on Windows</h2>
-					<ol class="eclipse-list">
-						<li>Download the Language Pack for the desired product and extract into your eclipse install.</li>
-						<li>Open the Regional and Language Options in the Control Panel and switch to the advanced tab.</li>
-						<li>Select the group 1 language you desire from the drop-down combo box.</li>
-						<li>Save changes and restart the system.</li>
-						<li>Launch Eclipse and select Window->Preferences->Report Design->Preview. Select the desired language from the drop-down combo box.</li>
-						<li>Restart Eclipse.</li>
-					</ol>
-
-					<a name="commonproblems"></a>
-					<h2 class="topic-section-header padding-top-small">Common Problems</h2>
-					<p>Some problems are due to cached information within Eclipse. Eclipse caches information about plugins for faster startup. Adding BIRT as we did above may result in stale cached information. Symptoms of this problem include:</p>
-					<ul class="eclipse-list">
-						<li>The BIRT perspective does not appear in Eclipse.</li>
-						<li>You receive "An error occurred" dialogs when opening reports or using the BIRT UI.</li>
-					</ul>
-
-					<h2 class="topic-section-header padding-top-small">Cleaning Cached Plugin Information</h2>
-					<p>The solution is to clean up the cached information. The recommended practice is to start Eclipse with the -clean option:</p>
-					<p>eclipse -clean</p>
-
-					<h2 class="topic-section-header padding-top-small">Cleaning the Cache on Windows</h2>
-					<p>If you are on Windows, and are not familiar with how to invoke Eclipse from the command line, do the following:</p>
-					<ul class="eclipse-list">
-						<li>Choose Start→All Programs→Accessories→Command Prompt</li>
-						<li>Move to the directory where you installed eclipse. If you installed Eclipse in the standard location, enter:
-							<ul class="eclipse-list">
-								<li>cd "\Program Files\eclipse"</li>
-							</ul>
-						</li>
-						<li>Run Eclipse with the -clean option:
-							<ul class="eclipse-list">
-								<li>eclipse -clean</li>
-							</ul>
-						</li>
-						<li>Eclipse will restart. Again check for the BIRT perspective.</li>
-					</ul>
 
 				</div>
 			</div>
