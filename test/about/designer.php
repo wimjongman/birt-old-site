1335b29db853dfae6922a40d602bddb67a8e603a
mwilliams1120 <mwilliams@actuate.com>
2014-04-10 15:52:56 -0500

    site refresh

--- test/about/designer.php
+++ test/about/designer.php
@@ -69,13 +69,30 @@ $PagePart 	= new PagePart();
 
 			        </div>
 
-					<h2 class="topic-section-header padding-top-small">Designer Install</h2>
+					<h2 class="topic-section-header padding-top-small">BIRT Report Designer</h2>
 					<p>BIRT can be downloaded and installed in different ways.
 					<ul class="eclipse-list">
-						<li>The complete BIRT designer or RCP Designer can be installed by downloading the required package here.</li>
-						<li>or by downloading the Eclipse IDE for Java and Report Developers Package</li>
-						<li>or through the update manager as described here. Make sure to use the latest Update Site.<br />Update Manager URLs </li>
-						<li>as a package that only contains the BIRT plugins (Framework) from the download page. Note additional requirements are located on the download page as well. These include dependencies on other Eclipse projects. These dependencies are listed on the download page for the specific build.</li>
+						<li>Data Explorer - Organizes your data sources (connections) and data sets (queries). The data set editor allows you to test your data set to ensure the report receives the correct data. Within this view multi dimensional cubes can be created using existing data sets. Cubes are currently used when building dynamic cross tables. This view also is used to design report parameters.</li>
+						<br />
+						<li>Navigator - Shows the projects and files that are in your workspace. You can also create projects, designs, and other BIRT files here.</li>
+						<br />
+						<li>Layout View - WYSIWYG editor that provides drag & drop creation of the presentation portion of your report.</li>
+						<br />
+						<li>Palette - Contains the standard BIRT report elements such as labels, tables, and charts and is used in conjunction with the Layout View to design reports.</li>
+						<br />
+						<li>Property Editor - Presents the most commonly used properties in a convenient format that makes editing quick and easy. BIRT also integrates with the standard Eclipse property view to provide a detailed listing of all properties for an item.</li>
+						<br />
+						<li>Script Editor - Scripting adds business logic to reports during data access, during report generation, or during viewing. The code editor provides standard Eclipse features for editing your scripts: syntax coloring, auto-complete and more. An interesting new feature, for BIRT 2.3 is the ability to debug scripts while the report is running.</li>
+						<br />
+						<li>Outline - BIRT reports are organized as a tree structure with the overall report as the root, and separate categories for styles, report content, data sources, data sets, report parameters and more. The Outline view provides a compact overview of your entire report structure.</li>
+						<br />
+						<li>Cheat Sheets - Learning a new tool is always a challenge, but Eclipse offers an innovative solution: cheat sheets. These are short bits of documentation that walk you through new tasks.</li>
+						<br />
+						<li>Resource Explorer - BIRT allows the reuse of report objects, such as tables, data sources and styles. Objects created for reuse are stored in a library file. To browse the contents of report libraries BIRT supplies a Resource Explorer view. This view list all libraries within the resource folder, in addition other shared content such as images and JavaScript files.</li>
+						<br />
+						<li>Chart Builder - Adding Charts to BIRT designs is expedited with the Chart Builder. Chart creation is separated into three phases: Select Chart Type, Select Data, and Format Chart.</li>
+						<br />
+						<li>Expression Builder - BIRT expressions are really just simple scripts that return a value. Expressions are used for assigning data values to report elements, building image locations, hyperlinks, parameter default values and many other places. Expressions are constructed within BIRT using the Expression Builder.</li>
 					</ul>
 					</p>
 
