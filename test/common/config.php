e510d2f0087ad0acf970058133b03ede2e6e8805
mwilliams1120 <mwilliams@actuate.com>
2014-04-10 00:39:24 -0500

    site update testing

--- test/common/config.php
+++ test/common/config.php
@@ -82,13 +82,13 @@ class PagePart {
 			<div class="actu-eclipse-container">
 				<div class="actu-eclipse-container-inner">	
 					<ul class="actu-eclipse-nav">
-						<li><a href="/about/">About</a></li>
-						<li><a href="/download/">Download</a></li>
-						<li><a href="/community/">Community</a></li>
-						<li><a href="/getting-started/">Getting Started</a></li>
-						<li><a href="/documentation/">Documentation</a></li>
-						<li><a href="/demos/">Demos</a></li>
-						<li><a href="/built-with-birt/">Built with BIRT</a></li>
+						<li><a href="'.$basePath.'/about/">About</a></li>
+						<li><a href="'.$basePath.'/download/">Download</a></li>
+						<li><a href="'.$basePath.'/community/">Community</a></li>
+						<li><a href="'.$basePath.'/getting-started/">Getting Started</a></li>
+						<li><a href="'.$basePath.'/documentation/">Documentation</a></li>
+						<li><a href="'.$basePath.'/demos/">Demos</a></li>
+						<li><a href="'.$basePath.'/built-with-birt/">Built with BIRT</a></li>
 					</ul>
 					<hr>
 					<a href="http://www.actuate.com/" ><img src="'.$basePath.'/img/logo/actuate-logo.png" /></a>
