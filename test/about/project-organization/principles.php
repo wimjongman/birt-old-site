c6f2a595c4d7fda26a66135e25f13b36b5ffe440
mwilliams1120 <mwilliams@actuate.com>
2014-04-11 12:28:06 -0500

    site facelift

--- test/about/project-organization/principles.php
+++ test/about/project-organization/principles.php
@@ -77,13 +77,18 @@ $PagePart 	= new PagePart();
 
 			        </div>
 
-			        <h2 class="topic-section-header padding-top-small">Designer Install</h2>
-					<p>BIRT can be downloaded and installed in different ways.
+			        <h2 class="topic-section-header padding-top-small">Project Principles</h2>
+					<p>Among the key principles on which this project has been initiated, and will be run, are the following:
 					<ul class="eclipse-list">
-						<li>The complete BIRT designer or RCP Designer can be installed by downloading the required package here.</li>
-						<li>or by downloading the Eclipse IDE for Java and Report Developers Package</li>
-						<li>or through the update manager as described here. Make sure to use the latest Update Site.<br />Update Manager URLs </li>
-						<li>as a package that only contains the BIRT plugins (Framework) from the download page. Note additional requirements are located on the download page as well. These include dependencies on other Eclipse projects. These dependencies are listed on the download page for the specific build.</li>
+					<li>Extension of the Eclipse value proposition - The Eclipse Project has set a high standard for technical excellence, functional innovation and overall extensibility within the Java IDE domain. We intend to apply these same standards to the business intelligence and reporting domain.</li><br />
+					<li>Leverage Eclipse ecosystem - A major goal of this project is to apply the application development strengths of the Eclipse Project to the business intelligence and reporting domain. The project will work closely with other Eclipse project areas whenever possible to leverage the capabilities being developed in those areas.</li><br />
+					<li>Vertical solutions - Reporting and Business intelligence are a facet of almost all applications that are developed today. A core principle of this project is to provide a general purpose reporting capability that can easily and seamlessly be integrated as a component of vertical applications and solutions.</li><br />
+					<li>Extensibility - To meet the varied requirements for reporting and Business Intelligence, provide extension points that enable developers to adapt the technology to meet application specific needs.</li><br />
+					<li>Vendor neutrality - We aim to encourage Eclipse participation and drive Eclipse market acceptance by providing vendor-neutral capabilities and to encourage participation for complementary capabilities through additional projects.</li><br />
+					<li>Open, standards-based development - Where market adopted standards exist that meet the design goals, our aim is to leverage and adhere to them. Where market adopted standards do not exist, we will develop and publish any new capabilities in the Eclipse open forum.</li><br />
+					<li>Incremental delivery - In order to meet the pent-up demand for reporting within the Java application market, a goal is to delivery functionality to market as rapidly as possible via an incremental delivery model.</li><br />
+					<li>Agile development - Our aim is to incorporate into our planning process the innovations that arise once a project is underway, and the feedback from our user community on our achievements to date. We think an agile development and planning process, in which progress is incremental, near-term deliverables are focused, and long-term planning is flexible, will be the best way to achieve this.</li><br />
+					<li>Inclusiveness & diversity - We aim to assimilate the best ideas from the largest number of participants representing the needs of the widest range of end-users. So we will encourage organizations across a broad range of technical, market and geographical domains to participate in this project.</li>
 					</ul>
 					</p>
 
