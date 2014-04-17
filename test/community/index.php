18745bbbd942e46e5c19592baae102a6ff266b72
mwilliams1120 <mwilliams@actuate.com>
2014-04-17 09:03:47 -0500

    typos

--- test/community/index.php
+++ test/community/index.php
@@ -66,11 +66,11 @@ $PagePart 	= new PagePart();
 					<h2 class="topic-section-header padding-top-small">Newsgroup</h2>
 					<p>The BIRT newsgroup is for users of the project to ask questions, discuss ideas and so on. Join in and get involved!
 					<ul class="eclipse-list">
-						<li><a href="http://www.eclipse.org/newsgroups" target="_blank">Information and Signup</a> Guidelines for using the newsgroups. Instructions for signing up.</li><br />
-						<li><a href="news://news.eclipse.org/eclipse.birt" target="_blank">news://news.eclipse.org/eclipse.birt</a> The easiest way to access the newsgroup is using your favorite news reader.</li><br />
-						<li><a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.birt" target="_blank">Simple Web Interface</a> If you don't have a newsreader, or can't access the NNTP server, use this simple web interface.</li><br />
-						<li><a href="http://dev.eclipse.org/newslists/news.eclipse.birt/maillist.html" target="_blank">Archive</a> Eclipse keeps a history of past newsgroup postings for your reference.</li><br />
-						<li><a href="http://www.eclipse.org/search/search.cgi?q=&cmd=Search%21&form=extended&wf=574a74&ps=10&m=all&t=5&ul=%2Fnewslists%2Fnews.eclipse.birt&wm=wrd&t=News&t=Mail" target="_blank">Search the BIRT Newsgroup Archives</a> for previously asked questions.</li>
+						<li><a href="http://www.eclipse.org/newsgroups" target="_blank">Information and Signup</a> - Guidelines for using the newsgroups. Instructions for signing up.</li><br />
+						<li><a href="news://news.eclipse.org/eclipse.birt" target="_blank">news://news.eclipse.org/eclipse.birt</a> - The easiest way to access the newsgroup is using your favorite news reader.</li><br />
+						<li><a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.birt" target="_blank">Simple Web Interface</a> - If you don't have a newsreader, or can't access the NNTP server, use this simple web interface.</li><br />
+						<li><a href="http://dev.eclipse.org/newslists/news.eclipse.birt/maillist.html" target="_blank">Archive</a> - Eclipse keeps a history of past newsgroup postings for your reference.</li><br />
+						<li><a href="http://www.eclipse.org/search/search.cgi?q=&cmd=Search%21&form=extended&wf=574a74&ps=10&m=all&t=5&ul=%2Fnewslists%2Fnews.eclipse.birt&wm=wrd&t=News&t=Mail" target="_blank">BIRT Newsgroup Archives</a> - Search for previously asked questions.</li>
 					</ul>
 					</p>
 					
