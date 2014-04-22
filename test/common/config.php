1cba0072c6e5b6418e97a18e4ab4ea8992f47dd5
mwilliams1120 <mwilliams@actuate.com>
2014-04-22 03:39:56 -0500

    fix page

--- test/common/config.php
+++ test/common/config.php
@@ -114,8 +114,8 @@ class PagePart {
 	function getFooter() {
 		$footer = '';
 		$footer = '<!-- Footer Begin -->
-			<div id="footer" class="content-area" style="margin-top:0px;">
-					<div class="content-area-inner" style="padding-top:30px; margin-bottom:20px;">
+			<div id="footer" class="content-area" style="margin-top:0px; padding-bottom:20px;">
+					<div class="content-area-inner" style="padding-top:30px;">
 <div class="col-3" style="width:150px;  margin-left:335px;">
 <ul>
 <li><a href="/">Home</a></li>
