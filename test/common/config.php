2a7981c45849fd6fe08522a2f1d7564e952341da
mwilliams1120 <mwilliams@actuate.com>
2014-04-22 03:36:52 -0500

    fix footer

--- test/common/config.php
+++ test/common/config.php
@@ -115,7 +115,7 @@ class PagePart {
 		$footer = '';
 		$footer = '<!-- Footer Begin -->
 			<div id="footer" class="content-area" style="margin-top:0px;">
-					<div class="content-area-inner" style="padding-top:30px; padding-bottom:30px;">
+					<div class="content-area-inner" style="padding-top:30px; margin-bottom:20px;">
 <div class="col-3" style="width:150px;  margin-left:335px;">
 <ul>
 <li><a href="/">Home</a></li>
