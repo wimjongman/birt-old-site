9ad3b08f45f0c474074de46ce4c2671bc84941b5
mwilliams1120 <mwilliams@actuate.com>
2014-04-22 03:24:20 -0500

    Footer fix

--- test/common/config.php
+++ test/common/config.php
@@ -116,7 +116,7 @@ class PagePart {
 		$footer = '<!-- Footer Begin -->
 			<div id="footer" class="content-area" style="margin-top:0px;">
 					<div class="content-area-inner" style="padding-top:30px;">
-<div class="col-3" style="width:150px;  margin-left:325px;">
+<div class="col-3" style="width:150px;  margin-left:311px;">
 <ul>
 <li><a href="/">Home</a></li>
 <li><a href="/legal/privacy.php">Privacy Policy</a></li>
@@ -127,8 +127,9 @@ class PagePart {
 <li><a href="/legal/copyright.php">Copyright Agent</a></li>
 <li><a href="/legal/">Legal</a></li>
 <li><a href="/org/foundation/contact.php">Contact Us</a></li></ul></div>
-			<div class="col-3" style="width:325px;">
-			<img style="width:175px;" src="/birt/test/img/logo/eclipse_logo_colour.png" /><p></p>
+			<div class="col-3" style="width:349px;">
+			<img style="width:175px;" src="/birt/test/img/logo/eclipse_logo_colour.png" />
+			<div class="clearfix"></div>
 			Copyright &copy; 2014 The Eclipse Foundation. All Rights Reserved.
 			</div>
 			</div>
