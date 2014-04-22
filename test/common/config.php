2b219c34c082d1a8d61ef282e76a377791899535
mwilliams1120 <mwilliams@actuate.com>
2014-04-22 00:23:53 -0500

    update test footer

--- test/common/config.php
+++ test/common/config.php
@@ -129,7 +129,7 @@ class PagePart {
 <li><a href="/legal/">Legal</a></li>
 <li><a href="/org/foundation/contact.php">Contact Us</a></li></ul></div>
 			<div class="col-4-fixed" style="width:400px">
-			<img style="width:200px; height:109px;" src="/eclipse.org-common/themes/Nova/images/eclipse-800x426.png);" />
+			<img src="/birt/test/img/Eclipse-Logo2014.png" />
 			<span>Copyright &copy; 2014 The Eclipse Foundation. All Rights Reserved.</span>
 			</div>
 			</div>
