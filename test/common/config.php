4426ddb2ce75605f8e95421ae5b5148e27374e32
mwilliams1120 <mwilliams@actuate.com>
2014-04-21 23:34:23 -0500

    footer test

--- test/common/config.php
+++ test/common/config.php
@@ -116,18 +116,22 @@ class PagePart {
 		$footer = '<!-- Footer Begin -->
 			<div id="footer" class="content-area" style="margin-top:0px;">
 					<div class="content-area-inner" style="padding-top:30px;">
-<div style="width:33%; display:inline;"><ul style="display:inline-block;">
+<div class="col-3"
+<ul>
 <li><a href="/">Home</a></li>
 <li><a href="/legal/privacy.php">Privacy Policy</a></li>
 <li><a href="/legal/termsofuse.php">Terms of Use</a></li>
 </ul></div>
-<div style="width:33%; display:inline"><ul style="display:inline-block;">
+<div class="col-3">
+<ul>
 <li><a href="/legal/copyright.php">Copyright Agent</a></li>
 <li><a href="/legal/">Legal</a></li>
 <li><a href="/org/foundation/contact.php">Contact Us</a></li></ul></div>
-			<div style=" width:33%; display:inline;">
-			<img style="width:200px; style="display:inline-block;" padding-bottom:20px;" src="/eclipse.org-common/themes/Nova/images/eclipse-800x426.png" />
+			<div class="col-3">
+			<img style="width:200px; padding-bottom:20px;" src="/eclipse.org-common/themes/Nova/images/eclipse-800x426.png" />
 			<span>Copyright &copy; 2014 The Eclipse Foundation. All Rights Reserved.</span>
+			</div>
+			</div>
 			</div>
 		<!-- Footer End -->';
 		return $footer;
