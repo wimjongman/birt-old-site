0f6f6b5ca653ef5fff5d20d82b529b9f66ce7ffa
mwilliams1120 <mwilliams@actuate.com>
2014-04-23 09:46:35 -0500

    fix title

--- test/documentation/building-birt.php
+++ test/documentation/building-birt.php
@@ -47,7 +47,7 @@ $PagePart 	= new PagePart();
 
 			<div class="dev-main eclipse-main">
 				<div class="eclipse-main-container">	
-					<h1 class="eclipse-title-main">Documentation</h1>
+					<h1 class="eclipse-title-main">Building BIRT</h1>
 					
 					<div class="topic-menu topic-menu-right">
                 
