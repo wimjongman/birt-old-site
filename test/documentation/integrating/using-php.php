753d3641747e65ede6eaf75a4f1fc4aea2ff13c6
mwilliams1120 <mwilliams@actuate.com>
2014-04-23 17:24:14 -0500

    site facelift

--- test/documentation/integrating/using-php.php
+++ test/documentation/integrating/using-php.php
@@ -72,20 +72,6 @@ $PagePart 	= new PagePart();
 
 			        </div>
 
-					<h2 class="topic-section-header padding-top-small">Integration Points</h2>
-					<p>BIRT supplies several APIs and an Example Java EE application for generating and viewing reports. The major APIs are the Design Engine API(DE API), Report Engine API(RE API) and the Chart Engine API (CE API). These pages describe the various APIs and illustrate examples on their usage. In addition to the APIs, BIRT supports scripting using Rhino within report designs. A primer is available to get started scripting.</p>
-					
-					<h2 class="topic-section-header padding-top-small">Example Report Viewer (Java EE App/Plugin)</h2>
-					<p>The BIRT Viewer can be used in a variety of ways:<br />
-					Note that the BIRT Viewer requires that cookies be enabled.
-					<ul class="eclipse-list">
-						<li>As a stand-alone, pre-built web application for running and viewing reports.</li><br />
-						<li>As a starter web application that you can customize to your needs.</li><br />
-						<li>Called through the BIRT tag library.</li><br />
-						<li>As a plugin in existing RCP applications.</li>
-					</ul>
-					</p>
-					
 					<h2 class="topic-section-header padding-top-small">Motivation</h2>
 					<p>BIRT is designed to be integrated into a Java EE web application. But, what if your chosen development environment is something else, such as PHP? Can you still use BIRT? Yes, you can.&nbsp; This page discusses how to use BIRT from PHP.</p>
 					
