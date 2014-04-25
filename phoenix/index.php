cf3b5af36410a597620564b6453ada89c38bca96
mwilliams1120 <mwilliams@actuate.com>
2014-04-25 11:39:08 -0500

    forward old site

--- phoenix/index.php
+++ phoenix/index.php
@@ -1,6 +1,6 @@
 <?php
 
-Header("Location: /birt")
+Header("Location: /birt/");
 
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
