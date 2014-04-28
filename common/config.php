61b426fcbfbcf34bc486930d17cfb3e0ba10d3b7
mwilliams1120 <mwilliams@actuate.com>
2014-04-28 15:24:36 -0500

    fix formatting

--- common/config.php
+++ common/config.php
@@ -94,8 +94,8 @@ class PagePart {
 					</ul>
 					<hr>
 					<span style="font-size:10px;">Project Sponsors:</span><br />
-					<a href="http://developer.actuate.com/" ><img src="'.$basePath.'/img/logo/actuate-logo.png" /></a><br />
-					<a href="http://www.innoventsolutions.com/birt-overview.html" ><img src="http://www.innoventsolutions.com/pics/logo_innovent.gif" /></a><br />
+					<a href="http://developer.actuate.com/" ><img src="'.$basePath.'/img/logo/actuate-logo.png" /></a><br /><br />
+					<a href="http://www.innoventsolutions.com/birt-overview.html" ><img src="http://www.innoventsolutions.com/pics/logo_innovent.gif" /></a><br /><br />
 					<a href="http://www.ibm.com/" ><img src="'.$basePath.'/img/logo/companies/ibm-logo.png" /></a>
 				</div>
 			</div>
