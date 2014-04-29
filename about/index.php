aada13c2489a6a0c90ebaf50abafffb99c8fd32f
mwilliams1120 <mwilliams@actuate.com>
2014-04-29 08:53:39 -0500

    formatting fix on about page

--- about/index.php
+++ about/index.php
@@ -70,18 +70,18 @@ $PagePart 	= new PagePart();
 			        <h3>BIRT: The Project</h3>
 					<p>BIRT is an open source software project that provides the BIRT technology platform to create data visualizations and reports that can be embedded into rich client and web applications, especially those based on Java and Java EE. BIRT is a top-level software project within the Eclipse Foundation, an independent not-for-profit consortium of software industry vendors and an open source community.</p>
 					<p>The project is sponsored by Actuate along with contributions from IBM, and Innovent Solutions. BIRT is supported by an active community of users here at Eclipse.org and at the <a href="http://developer.actuate.com" target="_blank">BIRT Developer Center</a>. It is licensed under the Eclipse Public License (EPL).</p>
-					<a href="project-organization.php">Project Organization</a> | <a target="_blank" href="https://wiki.eclipse.org/BIRT_Project_Plan">Project Development Plan</a>
+					<p><a href="project-organization.php">Project Organization</a> | <a target="_blank" href="https://wiki.eclipse.org/BIRT_Project_Plan">Project Development Plan</a></p>
 					
 					<h3>BIRT: The Groundswell</h3>
 					<p>Today, BIRT technology platform is one of the most widely adopted data visualization and reporting technologies with over 12 million downloads and over 2.5M developers across 157 countries. BIRT also has a large, active and growing developer community representing all types of organizations. Major technology companies such as IBM, Cisco, S1 and ABS Nautical Systems have incorporated BIRT into their product lines.<br /><br />
 					<a href="http://www.actuate.com/partners/birt-adopters/" target="_blank">See a list of BIRT Adopters</a></p>
 					
-					<iframe width="560" height="315" src="//www.youtube.com/embed/_gvL58G41CA" frameborder="0" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="//www.youtube.com/embed/_gvL58G41CA" frameborder="0" allowfullscreen></iframe><br />
 
 					<h3>BIRT: The Technology Platform</h3>
 					<p>BIRT has two main components: a visual report designer for creating BIRT Designs, and a runtime component for generating those designs that can be deployed to any Java environment. The BIRT project also includes a charting engine that is both fully integrated into the BIRT designer and can be used standalone to integrate charts into an application.</p>
 					<p>BIRT designs are persisted as XML and can access a number of different data sources including JDO datastores, JFire Scripting Objects, POJOs, SQL databases, Web Services and XML.</p>
-					<a href="architecture.php">Learn more about BIRT Architecture</a>
+					<p><a href="architecture.php">Learn more about BIRT Architecture</a></p>
 
 					<h3>BIRT: The Philosophy</h3>
 					<p>BIRT is about open standards. It integrates with any data source in any environment. BIRT represents a commitment to the highest standards in software design and quality.</p>
