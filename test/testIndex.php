180230b544db02d637f46c1317e2a39ae23f0e56
mwilliams1120 <mwilliams@actuate.com>
2014-10-31 15:39:28 -0500

    testing

--- test/testIndex.php
+++ test/testIndex.php
@@ -10,10 +10,18 @@ $pageTitle 		= "Eclipse BIRT Test Home";
 	$pageAuthor		= "";
 
 $html = <<<EOHTML
-$PagePart->getAfterBody();
+<!-- Google Tag Manager -->
+<noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-PW8LFV\"
+height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PW8LFV');</script>
+<!-- End Google Tag Manager -->
 
-<div id="maincontent" style="background-color:#f4f4f4;">
-        <div id="midcolumn">
+<div id="maincontent">
+        <div id="midcolumn" style="background-color:#f4f4f4;">
 <div>
 	<div class="content-area">
 		<div class="content-area-inner" style="padding-top:40px;">
